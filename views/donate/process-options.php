9474afcc00391676202d0c9fe242c85d107263a5
Itay Banner <itay@ibanner.co.il>
2017-06-05 12:12:15 +0300

    markup & styles for donation options tabs

--- views/donate/process-options.php
+++ views/donate/process-options.php
@@ -13,16 +13,54 @@
         <!-- Option panes -->
         <div class="tab-content">
             <div role="tabpanel" class="tab-pane container active" id="online">
-                <div class="container">Online</div>
+                <p>
+                    <?php
+                        $url = 'https://co.clickandpledge.com/sp/d1/default.aspx?wid=39977';
+                        $link = sprintf( wp_kses( __( '<strong>Your gift is tax deductible in Israel.</strong> To make a tax deductible gift in the United States <a href="%s" target="_blank" rel="nofollow">click here</a>.', 'BH' ), array(  'a' => array( 'href' => array() ) , 'strong' => array()) ), esc_url( $url ) );
+                        echo $link;
+
+                        get_template_part('views/donate/form', 'amount');
+                        get_template_part('views/donate/form', 'details');
+                    ?>
+                </p>
             </div>
             <div role="tabpanel" class="tab-pane container" id="check">
-                <div class="container">Check</div>
+                <p>
+                    <strong><?php esc_html_e('Please make check payable to Beit Hatfutsot and send to:', 'BH'); ?></strong>
+                </p>
+                <address>
+                    <?php
+                    $email = 'racheli@bh.org.il';
+                    $email_link = '<a href="' . $email . '">' . $email . '</a>';
+                    $address = sprintf(
+                            __('P.O.B %1$s Tel-Aviv<br/>%2$s Israel<br/>For more information please contact us at %3$s' , 'BH'),
+                            '39359' , '6139202', $email_link );
+                    echo $address;
+                    ?>
+                </address>
             </div>
             <div role="tabpanel" class="tab-pane container" id="transfer">
-                <div class="container">Transfer</div>
+                <p>
+                    <strong><?php esc_html_e('If you wish to make a bank transfer from within Israel:', 'BH'); ?></strong>
+                </p>
+                <p>
+                    <?php
+                    $email = 'racheli@bh.org.il';
+                    $email_link = '<a href="' . $email . '">' . $email . '</a>';
+                    $address = sprintf( __('Bank Mizrachi-Tefahot<br/>Branch: %1$s<br/>Account: %2$s' , 'BH'), '493' , '387660' );
+                    echo $address;
+                    ?>
+                </p>
+                <p>
+                    <?php echo __('For more information please contact us at', 'BH') . ' ' . $email_link; ?>
+                </p>
             </div>
             <div role="tabpanel" class="tab-pane container" id="phone">
-                <div class="container">By Phone</div>
+                <div class="container">
+                    <p>
+                        <strong><?php _e('Please call us on this number:<br/>+972 3 7457841', 'BH'); ?></strong>
+                    </p>
+                </div>
             </div>
         </div>
 
