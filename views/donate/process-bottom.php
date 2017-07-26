a1082b043da4c14e15a1de670d0c473015229384
Itay Banner <itay@ibanner.co.il>
2017-07-26 10:51:47 +0300

    Financial docs fields and display

--- views/donate/process-bottom.php
+++ views/donate/process-bottom.php
@@ -2,30 +2,28 @@
 
     <div class="container">
 
-        <h4>Lorem Ipsum</h4>
+        <h4><?php esc_html_e('Financial Information', 'BH') ?></h4>
         <div class="row">
-            <div class="col-sm-4">
-                <a href="#" >Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</a>
-            </div>
-            <div class="col-sm-4">
-                <a href="#" >Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</a>
-            </div>
-            <div class="col-sm-4">
-                <a href="#" >Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</a>
-            </div>
-        </div>
 
-        <h4>Lorem Ipsum 2</h4>
-        <div class="row">
-            <div class="col-sm-4">
-                <a href="#" >Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</a>
-            </div>
-            <div class="col-sm-4">
-                <a href="#" >Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</a>
-            </div>
-            <div class="col-sm-4">
-                <a href="#" >Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</a>
-            </div>
+            <?php
+            $docs = array(
+                    'proper_management_approval' => esc_html__('Proper Management Approval Document', 'BH'),
+                    'article46' => esc_html__('IRS public institution & article 46 Approval', 'BH'),
+                    'annual_report' => esc_html__('Annual Report', 'BH'),
+            );
+
+            foreach ($docs as $doc => $doc_title) {
+
+                $doc_file = get_field($doc, 'option');
+                $doc_year = get_field($doc . '_year', 'option');
+                if ( $doc_year ) {
+                    $doc_title = $doc_title. ' - ' . $doc_year;
+                }
+                $tooltip = ( $doc_file ? esc_html__('Click to download the document', 'BH') : esc_html__('File Unavailable', 'BH') );
+
+                echo '<div class="col-sm-4"><a href="' . $doc_file . '" role="link" class="' . ( $doc_file ? '' : 'file-na' ) . '" title="' . $tooltip . '" download>' . $doc_title . ' ' . esc_html__('(PDF)', 'BH')  . '</a></div>';
+            }
+            ?>
         </div>
 
 
