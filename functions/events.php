efa25967200cc7a03297148b9aec3c040db1d863
Nir Goldberg <nir@htmline.com>
2019-09-10 14:33:37 +0300

    Event order link - remove ticketnet link modification, support smarticket from now on

--- functions/events.php
+++ functions/events.php
@@ -4,7 +4,7 @@
  *
  * @author		Beit Hatfutsot
  * @package		bh/functions
- * @version		2.8.0
+ * @version		2.14.2
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
@@ -286,21 +286,21 @@ function BH_get_event_purchase_btn($event_id) {
 		
 			// regular event
 			if ($ticket_net_link) :
-				$btn = '<a class="btn green-btn event-ticket-net-link" onclick="BH_tickets.ticketnet_purchase_link(\'' . $ticket_net_link . '\')">' . $order_tickets_btn . '</a>';
+				$btn = '<a class="btn green-btn event-ticket-net-link" href="' . $ticket_net_link . '" target="_blank">' . $order_tickets_btn . '</a>';
 			endif;
 			
 		else :
 		
 			// series of events
 			if ($ticket_net_link && $today <= $start_date) :
-				$btn = '<a class="btn green-btn event-ticket-net-link" onclick="BH_tickets.ticketnet_purchase_link(\'' . $ticket_net_link . '\')">' . $purchase_series_btn . '</a>';
+				$btn = '<a class="btn green-btn event-ticket-net-link" href="' . $ticket_net_link . '" target="_blank">' . $purchase_series_btn . '</a>';
 			else :
 				foreach ($series as $event) :
 					$event_date				= $event['date'];
 					$event_ticket_net_link	= $event['ticket_net_link'];
 					
 					if ($today <= $event_date && $event_ticket_net_link) :
-						$btn = '<a class="btn green-btn event-ticket-net-link" onclick="BH_tickets.ticketnet_purchase_link(\'' . $event_ticket_net_link . '\')">' . $order_tickets_btn . '</a>';
+						$btn = '<a class="btn green-btn event-ticket-net-link" href="' . $event_ticket_net_link . '" target="_blank">' . $order_tickets_btn . '</a>';
 						break;
 					endif;
 				endforeach;
--- views/landing/order-btn.php
+++ views/landing/order-btn.php
@@ -5,7 +5,7 @@
  * @author		Beit Hatfutsot
  * @package		bh/views/landing
  * @since		2.8.0
- * @version		2.13.0
+ * @version		2.14.2
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 <?php if ( $order_target == 'link' ) : ?>
 
-	<a class="btn event-ticket-net-link" onclick="BH_tickets.ticketnet_purchase_link('<?php echo $order_link; ?>')"><?php echo $order_text; ?></a>
+	<a class="btn event-ticket-net-link" href="<?php echo $order_link; ?>" target="_blank"><?php echo $order_text; ?></a>
 
 <?php elseif ( $order_target == 'form' ) : ?>
 
