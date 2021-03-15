b054222ccd70e77f690ffa966a8a08a426a08d40
Nir Goldberg <nir@htmline.com>
2021-03-15 12:44:00 +0200

    Modify logo link to target website main home page

--- views/components/anu-logo.php
+++ views/components/anu-logo.php
@@ -5,7 +5,7 @@
  * @author 		Beit Hatfutsot
  * @package 	bh/views/components
  * @since		3.0
- * @version     3.1.1
+ * @version     3.1.2
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
@@ -24,7 +24,7 @@ if ( ! $wpml_lang )
 
 <div class="logo">
 
-	<a href="<?php echo HOME; ?>" title="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>">
+	<a href="<?php echo apply_filters( 'wpml_permalink', get_bloginfo( 'url' ) . '/home', $wpml_lang ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>">
 
 		<?php get_template_part( 'views/svgs/shape', 'anu-logo-' . $wpml_lang ); ?>
 
